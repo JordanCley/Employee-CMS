08c7fd42f135e8f5b650ce8bf11b9f3012842423
Jordan McQuiston <jordan@jordanmcquiston.com>
2019-11-27 17:48:53 -0800

    Got methods in class working by adding util.promisfy to connection query.THen made all functions async and now can add a new employee to the db. Can now view all employees,deps, and roles, as well as quit program

--- Employee.js
+++ Employee.js
@@ -3,29 +3,38 @@ class Employee {
     this.firstName = firstName;
     this.lastName = lastName;
     this.role_id = null;
-    this.department_id = null;
+    this.manager_id = null;
   }
-  getRoleID(connection, answers) {
+  async getRoleID(connection, answers) {
     // query DB and set role id to id of role
-    const query = `SELECT id FROM roles WHERE ?;`;
-    connection.query(query, { title: answers.role }, (err, data) => {
-      if (err) throw err;
-      this.role_id += data[0].id;
-      console.log(this.role_id);
-      return this;
-    });
+    const query = "SELECT id FROM roles WHERE ?;";
+    const result = await connection.query(query, { title: answers.role });
+    this.role_id += result[0].id;
+    console.log(this.role_id);
+    return this;
   }
 
-  getDepID(connection, answers) {
+  async getDepID(connection, answers) {
     // query DB and get Dep ID of Department
-    const query = `SELECT id FROM departments WHERE ?;`;
-    connection.query(query, { name: answers.department }, (err, data) => {
-      if (err) throw err;
-      this.department_id = data[0].id;
-      console.log(this.department_id);
-      console.log(this);
+    const query = "SELECT id FROM departments WHERE ?;";
+    const result = await connection.query(query, { name: answers.department });
+    this.manager_id = result[0].id;
+    console.log(this.manager_id);
+    //   console.log(this);
+    return this;
+  }
+
+  async postToDB(connection){
+    //    const query = 
+      await connection.query("INSERT INTO employees SET ?",  
+      {
+          first_name: this.firstName,
+          last_name: this.lastName,
+          role_id: this.role_id,
+          manager_id: this.manager_id
+      });
+      console.log("Employeed added to the database")
       return this;
-    });
   }
 }
 
